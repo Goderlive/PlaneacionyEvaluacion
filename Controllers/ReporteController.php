53b772937d98040c930b6a5434f7d33a4fc72b1e
German Guillen <goder@live.com>
2022-09-26 17:57:01 -0500

    boton de avance en avances

--- Controllers/ReporteController.php
+++ Controllers/ReporteController.php
@@ -154,9 +154,11 @@ function Actividades($con, $mes, $id_area, $meses, $actividadesDB){
             <td class="px-6 py-4">'.
                 $a[$mesi]
             .'</td>
-            <td class="px-6 py-4">'.
-                $avanceThisMes
-            .'</td>
+            <td class="px-6 py-4">
+                <button class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button" data-modal-toggle="defaultModal">'.
+                        $avanceThisMes
+                .'</button>
+            </td>
             <td class="px-6 py-4 text-right">
                 <button ' . $botones[0] . 'type="button" data-modal-toggle="mymodal'. $a['codigo_actividad'] .'"> '.
                     $botones[1]
