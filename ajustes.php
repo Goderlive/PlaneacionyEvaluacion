525df9072932e51c3dde946386d5afa4170d75a4
German Guillen <goder@live.com>
2024-04-11 18:17:54 -0600

    Avandes en tenancingo

--- ajustes.php
+++ ajustes.php
@@ -157,7 +157,7 @@ if ($_SESSION['sistema'] == "pbrm") :
                                         </svg> Agregar
                                     </button>
                                 <?php else : ?>
-                                    <form action="models/ajustes_model.php" method="POST">
+                                    <form action="" method="POST">
                                         <input type="hidden" name="uippe" value="true">
                                         <input type="submit" name="delete" value="Eliminar" class="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">
                                     </form>
@@ -190,7 +190,7 @@ if ($_SESSION['sistema'] == "pbrm") :
                                         </svg> Agregar
                                     </button>
                                 <?php else : ?>
-                                    <form action="models/ajustes_model.php" method="POST">
+                                    <form action="" method="POST">
                                         <input type="hidden" name="teso" value="true">
                                         <input type="submit" name="delete" value="Eliminar" class="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">
                                     </form>
@@ -337,7 +337,7 @@ if ($_SESSION['sistema'] == "pbrm") :
                         $titulares = TraeDirectores($con);
                         ?>
                         Elige entre las opciones existentes <br>
-                        <form action="models/ajustes_model.php" method="POST">
+                        <form action="" method="POST">
 
                             <select name="id_uippe" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
 
@@ -381,7 +381,7 @@ if ($_SESSION['sistema'] == "pbrm") :
                         $titulares = TraeDirectores($con);
                         ?>
                         Elige entre las opciones existentes <br>
-                        <form action="models/ajustes_model.php" method="POST">
+                        <form action="" method="POST">
 
                             <select name="id_teso" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
 
