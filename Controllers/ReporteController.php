c46ab9b8a8b37be36b7ecc98817b4d8b8a9c5c25
German Guillen <goder@live.com>
2024-05-27 14:06:01 -0600

    desde metepec

--- Controllers/ReporteController.php
+++ Controllers/ReporteController.php
@@ -531,7 +531,7 @@ function lista_localidades($con){
     <select multiple id="localidades" required name="localidades[]" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" onchange="calcularSumatoria()">
         '.$options.'
     </select>
-    <p>Beneficiarios Indirectos: <span id="sumatoria"></span></p>
+    <p>Localidades beneficiadas: <span id="sumatoria"></span></p>
     '.$script;
 }
 
